c6de30a75cf9aa64576b89a5eaa7b9d5cee70a3a
julianschievelkamp <julian.schievelkamp@gmail.com>
2022-06-24 14:10:45 +0200

    update divider height

--- src/components/Layout/Header/styles.js
+++ src/components/Layout/Header/styles.js
@@ -1,17 +1,30 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import { softShadow } from "styles/variables";
+
+const fadeIn = keyframes`
+    0% { width: 0; }
+    100% { width: 100%; }
+`;
 
 export const StyledHeader = styled.div`
     height: 10rem;
     padding: 1rem;
+    padding-bottom: 0.5rem;
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
 `;
 
 export const Divider = styled.div`
-    border-bottom: 2px solid rgb(0, 0, 0, 0.2);
-    width: 100%;
+    border-bottom: 1px solid rgb(0, 0, 0, 0.3);
+    width: 0;
     margin-top: 1rem;
+
+    animation-name: ${fadeIn};
+    animation-fill-mode: forwards;
+    animation-duration: 1s;
+    animation-delay: 0.3s;
+    box-shadow: ${softShadow};
 `;
 
 export const Flex = styled.div`
